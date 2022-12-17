179911bc0d6c49400bfaf571b1cfb90356831e6b
asma sadqi <asma.sadqi@gmail.com>
2022-12-17 15:30:35 +0100

    update

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "tasks", to: "tasks#all", as: :index  
-  get "tasks/new", to: "tasks#new", as: :new
-  post "tasks", to: "tasks#create"
-  get "tasks/:id", to: "tasks#show", as: :task
+
+  # Read all
+  get    "tasks",          to: "tasks#all"
+  # Create
+  get    "tasks/new",      to: "tasks#new",  as: :new
+  post   "tasks",          to: "tasks#create"
+  # Read one - The `show` route needs to be *after* `new` route.
+  get    "tasks/:id",      to: "tasks#show", as: :task
+  # Update
+  get    "tasks/:id/edit", to: "tasks#edit", as: :edit
+  patch  "tasks/:id",      to: "tasks#update"
+  # Delete
+  delete "tasks/:id",      to: "tasks#destroy"
 end
